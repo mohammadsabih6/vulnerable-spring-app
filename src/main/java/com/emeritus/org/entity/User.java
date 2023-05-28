6eb8c69880f1479ac91c4b43f0aa2f68be871bdd
mohammadsabih6 <sabih.tariq@xloopdigital.com>
2023-05-28 13:24:55 +0500

    Updated Code

--- src/main/java/com/emeritus/org/entity/User.java
+++ src/main/java/com/emeritus/org/entity/User.java
@@ -30,9 +30,6 @@ public class User {
 
 
     private String password;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     private Set<String> roles = new HashSet<>();
     public User() {
     }
@@ -43,43 +40,5 @@ public class User {
         this.password = password;
     }
 
-    // public Long getId() {
-    //     return id;
-    // }
-
-    // public void setId(Long id) {
-    //     this.id = id;
-    // }
-
-    // public String getUsername() {
-    //     return username;
-    // }
-
-    // public void setUsername(String username) {
-    //     this.username = username;
-    // }
-
-    // public String getEmail() {
-    //     return email;
-    // }
-
-    // public void setEmail(String email) {
-    //     this.email = email;
-    // }
-
-    // public String getPassword() {
-    //     return password;
-    // }
-
-    // public void setPassword(String password) {
-    //     this.password = password;
-    // }
-    // public Set<String> getRoles() {
-    //     return roles;
-    // }
-
-    // public void setRoles(Set<String> roles) {
-    //     this.roles = roles;
-    // }
 
 }
