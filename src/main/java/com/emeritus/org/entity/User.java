c0b4e92d80eb700fdd652768a33c764ce670b628
mohammadsabih6 <sabih.tariq@xloopdigital.com>
2023-05-29 16:22:19 +0500

    DOne

--- src/main/java/com/emeritus/org/entity/User.java
+++ src/main/java/com/emeritus/org/entity/User.java
@@ -1,44 +1,40 @@
 package com.emeritus.org.entity;
-
-
-
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
 @Setter
 @Getter
 @Entity
-@Table(	name = "users",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = "username"),
-                @UniqueConstraint(columnNames = "email")
-        })
+@Table(name = "users")
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
     private String username;
-
-
     private String email;
-
-
     private String password;
+    //this help taken from internet
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     private Set<String> roles = new HashSet<>();
+
     public User() {
     }
-
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
-
-
+    public Set<String> getRoles() {
+        return roles;
+    }
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
 }
--- src/main/java/com/emeritus/org/security/services/UserDetailsImpl.java
+++ src/main/java/com/emeritus/org/security/services/UserDetailsImpl.java
@@ -1,29 +1,21 @@
 package com.emeritus.org.security.services;
-
 import com.emeritus.org.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 public class UserDetailsImpl implements UserDetails {
     private static final long serialVersionUID = 1L;
-
     private Long id;
     private String username;
     private String email;
-
     @JsonIgnore
     private String password;
-
     private Set<GrantedAuthority> authorities;
-
     public UserDetailsImpl(Long id, String username, String email, String password,
                            Collection<? extends String> authorities) {
         this.id = id;
@@ -34,7 +26,6 @@ public class UserDetailsImpl implements UserDetails {
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toSet());
     }
-
     public static UserDetailsImpl build(User user) {
         return new UserDetailsImpl(
                 user.getId(),
@@ -44,45 +35,36 @@ public class UserDetailsImpl implements UserDetails {
                 user.getRoles()
         );
     }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
     }
-
     public Long getId() {
         return id;
     }
-
     public String getEmail() {
         return email;
     }
-
     @Override
     public String getPassword() {
         return password;
     }
-
     @Override
     public String getUsername() {
         return username;
     }
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
     @Override
     public boolean isEnabled() {
         return true;
